f7482b81ea8f00e05a121e1462e2eee475280b58
kiittyka <b8ibmjava22@iiht.tech>
2019-11-04 14:48:23 +0530

    Added email validation

--- spring-mvc/src/main/java/com/mycompany/spring_mvc/model/Customer.java
+++ spring-mvc/src/main/java/com/mycompany/spring_mvc/model/Customer.java
@@ -1,13 +1,11 @@
 package com.mycompany.spring_mvc.model;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.Email;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
